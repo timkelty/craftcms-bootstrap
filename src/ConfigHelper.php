8de46fd28565eabd3147c2fec16c65d44c4167fb
Tim Kelty <tim@fusionary.com>
2018-02-26 11:20:25 -0500

    More explicit names

--- src/ConfigHelper.php
+++ src/ConfigHelper.php
@@ -9,17 +9,17 @@ class ConfigHelper
 {
     const ENV_PREFIX = 'CRAFT_';
 
-    public static function getSetting($name, $default = null, $envPrefix = self::ENV_PREFIX)
+    public static function getEnvSetting($name, $default = null, $envPrefix = self::ENV_PREFIX)
     {
         $envVar = Stringy::create($name)->underscored()->toUpperCase()->prepend($envPrefix);
 
         return Environment::get($envVar, $default);
     }
 
-    public static function mapSettings($config, $envPrefix = self::ENV_PREFIX)
+    public static function mapEnvSettings($config, $envPrefix = self::ENV_PREFIX)
     {
         return Collection::make($config)->map(function ($value, $name) use ($envPrefix) {
-            return static::getSetting($name, $value, $envPrefix);
+            return static::getEnvSetting($name, $value, $envPrefix);
         })->all();
     }
 }
